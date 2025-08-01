ce49820170facebd9fcac18f5c47f3c5621ca238
Niteshkumarjain-ui <niteshjain8769@gmail.com>
2025-08-02 00:15:39 +0530

    ordering tag in swagger

--- src/main/java/com/learn/splitwise/config/OpenApiConfig.java
+++ src/main/java/com/learn/splitwise/config/OpenApiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -13,7 +14,14 @@ import org.springframework.context.annotation.Configuration;
                 title = "Splitwise Apis",
                 version = "1.0",
                 description = "Backend API for managing group expenses and balances"
-        )
+        ),
+        tags = {
+                @Tag(name = "Health", description = "Health Check APIs"),
+                @Tag(name = "User", description = "User Management APIs"),
+                @Tag(name = "Group", description = "Group Management APIs"),
+                @Tag(name = "Expense", description = "Expense Management APIs"),
+                @Tag(name = "Settle", description = "Settle Management APIs")
+        }
 )
 @SecurityScheme(
         name = "bearerAuth",
--- src/main/java/com/learn/splitwise/controller/AuthController.java
+++ src/main/java/com/learn/splitwise/controller/AuthController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Authentication")
+@Tag(name = "User")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
